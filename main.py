80cf95e8eccf8dd182ad2e3f3dd46c91eccf9c68
Nathan Kang <nathankang@Nathans-MacBook-Pro-5.local>
2022-11-17 09:34:43 -0800

    created template for the main file

---
+++ main.py
@@ -0,0 +1,13 @@
+from config import *
+# import a UserInterface class
+from game import Game
+# import a Bot class
+
+ui = UserInterface()  # place holder, should be a subclass
+game = Game()
+bot = Bot()  # place holder, should be a subclass
+
+ui.set_game(game)
+ui.set_players(HUMAN_PLAYER, bot)
+
+ui.run_loop()
