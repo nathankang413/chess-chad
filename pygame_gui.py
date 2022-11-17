8b7b0932930c92151e600d096b47730c000ce699
Nathan Kang <nathankang@Nathans-MacBook-Pro-5.local>
2022-11-17 10:37:37 -0800

    restructured pygame script into PygameGUI class

--- pygame_gui.py
+++ pygame_gui.py
@@ -1,37 +1,50 @@
 # Import and initialize the pygame library
 import pygame
 from config import *
+from game import Game
+from user_interface import UserInterface
 
-# Set up the drawing window
-# flags = pygame.RESIZABLE
-flags = pygame.SCALED
-screen = pygame.display.set_mode(size=[BOARD_SIZE, BOARD_SIZE], flags=flags, display=0, vsync=0)
+class PygameGUI(UserInterface):
+    """
+    A UserInterface implemented with PyGame
+    """
 
-# pygame.display.set_icon()
-# pygame.display.set_caption()
+    def __init__(self, game: Game = None, players: tuple = None) -> None:
+        super().__init__(game, players)
 
-# Run until the user asks to quit
-running = True
-while running:
+    def set_game(self, game: Game) -> None:
+        super().set_game(game)
 
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    def set_players(self, *players) -> None:
+        super().set_players(*players)
 
-    # Fill the background with white
-    screen.fill((255, 255, 255))
+    def run_loop(self) -> None:
+        super().run_loop()
 
-    for x in range(8):
-        for y in range(8):
-            if (x + y) % 2 == 0:
-                color = (0, 144, 0)
-            else:
-                color = (144, 0, 0)
-            pygame.draw.rect(screen, color, (x * SQUARE_SIZE, y * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+        # init display and screen
+        screen = pygame.display.set_mode(size=[BOARD_SIZE, BOARD_SIZE], flags=pygame.SCALED)
+        pygame.display.set_icon()
+        pygame.display.set_caption()
 
-    # Flip the display
-    pygame.display.flip()
+        # game loop
+        running = True
+        while running:
 
-# Done! Time to quit.
-pygame.display.quit()
+            # event handling
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            # checker board
+            for x in range(8):
+                for y in range(8):
+                    if (x + y) % 2 == 0:
+                        color = (0, 144, 0)
+                    else:
+                        color = (144, 0, 0)
+                    pygame.draw.rect(screen, color, 
+                        (x * SQUARE_SIZE, y * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            
+            pygame.display.flip()
+        
+        pygame.display.quit()
