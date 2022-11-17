18a394c8378c155a78cb1fda0abcdfcbe2a6295a
Nathan Kang <nathankang@Nathans-MacBook-Pro-5.local>
2022-11-17 09:17:09 -0800

    moved GameState class to a different file

--- game.py
+++ game.py
@@ -1,17 +1,4 @@
-class GameState:
-    """
-    A data structure containing the static state of a chess game
-    """
-
-    def __init__(self, board: list[list], turn: bool, check: int) -> None:
-        """
-        Creates a new GameState
-        board - a 2d matrix of piece positions
-        turn - True if white to move
-        check - see config for check values
-        """
-        pass
-
+from game_state import GameState
 
 class Game:
     """
