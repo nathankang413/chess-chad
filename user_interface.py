3107720d1532a7596bb1a662c34f4830b64cfd22
Nathan Kang <nathankang@Nathans-MacBook-Pro-5.local>
2022-11-17 08:22:39 -0800

    created abstract UserInterface class to be implemented by the pygame GUI, terminal I/O, or browser GUI

---
+++ user_interface.py
@@ -0,0 +1,16 @@
+class UserInterface:
+    """
+    Abstract class for interfacing with the user for the Chess game
+    Handles the main game loop and I/O with the user
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def run_loop(self) -> None:
+        """
+        Runs the game loop for a chess game 
+        Handles I/O with the user
+        Handles interfacing with chess engine
+        """
+        pass
