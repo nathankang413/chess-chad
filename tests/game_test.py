83dea3a1755bcde512a3de068f963540a8ba49b8
Nathan Kang <nathankang@Nathans-MacBook-Pro-5.local>
2022-11-17 16:20:31 -0800

    removed unnecessary files

--- tests/game_test.py
+++ tests/game_test.py
@@ -1,15 +1,40 @@
 import unittest
 import sys
 sys.path.append('../')
-from src.game import *
+from src.game import Game
 
 class TestGameClass(unittest.TestCase):
 
-    def test_01(self):
+    def test_00_empty(self):
         pass
 
-    def _check_state(self, game, board, turn, check):
-        self.assertEqual(game.get_board())
+    def test_01_FEN_to_board(self):
+        pass
+
+    def test_02_board_to_FEN(self):
+        pass
+
+    def test_03_check_valid_board(self):
+        pass
+
+    def test_11_set_state(self):
+        pass
+
+    def test_12_get_check(self):
+        pass
+
+    def test_13_get_legal_moves(self):
+        pass
+
+    def test_14_make_move(self):
+        pass
+
+    def test_15_get_history(self):
+        pass
+
+    def _check_state(self, game: Game, board: list[list], turn: bool):
+        self.assertEqual(game.get_board(), board)
+        self.assertEqual(game.get_turn(), turn)
 
 
 if __name__ == '__main__':
